ce76b43db48843a16ebec726500c0af9c0c8e347
Li Donghua <donghua.li@dianping.com>
2014-01-30 13:57:17 +0800

    Authenticate users to log them in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'store#index', as: 'store'
 
   # Example of regular route:
@@ -21,9 +27,6 @@ Depot::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  resources :products do
-    get :who_bought, on: :member
-  end
 
   # Example resource route with options:
   #   resources :products do
